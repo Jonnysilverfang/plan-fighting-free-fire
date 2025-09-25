6f4b058f779a05b91e34e5e7637524e805816182
kiennguly <24520885@gm.uit.edu.vn>
2025-09-25 23:50:59 +0700

    Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -13,48 +13,12 @@ namespace Kien
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            // Placeholder Username
             textBoxUser.Text = "Tên đăng nhập";
             textBoxUser.ForeColor = Color.Gray;
-
             textBoxPass.Text = "Mật khẩu";
             textBoxPass.ForeColor = Color.Gray;
             textBoxPass.UseSystemPasswordChar = false;
-
-            textBoxUser.Enter += (s, ev) =>
-            {
-                if (textBoxUser.Text == "Tên đăng nhập")
-                {
-                    textBoxUser.Text = "";
-                    textBoxUser.ForeColor = Color.Black;
-                }
-            };
-            textBoxUser.Leave += (s, ev) =>
-            {
-                if (string.IsNullOrWhiteSpace(textBoxUser.Text))
-                {
-                    textBoxUser.Text = "Tên đăng nhập";
-                    textBoxUser.ForeColor = Color.Gray;
-                }
-            };
-
-            textBoxPass.Enter += (s, ev) =>
-            {
-                if (textBoxPass.Text == "Mật khẩu")
-                {
-                    textBoxPass.Text = "";
-                    textBoxPass.ForeColor = Color.Black;
-                    textBoxPass.UseSystemPasswordChar = true;
-                }
-            };
-            textBoxPass.Leave += (s, ev) =>
-            {
-                if (string.IsNullOrWhiteSpace(textBoxPass.Text))
-                {
-                    textBoxPass.Text = "Mật khẩu";
-                    textBoxPass.ForeColor = Color.Gray;
-                    textBoxPass.UseSystemPasswordChar = false;
-                }
-            };
         }
 
         private void buttonRegister_Click(object sender, EventArgs e)
@@ -64,22 +28,20 @@ namespace Kien
 
             if (user == "Tên đăng nhập" || pass == "Mật khẩu")
             {
-                MessageBox.Show("Vui lòng nhập đầy đủ!", "Lỗi",
-                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Vui lòng nhập đầy đủ!");
                 return;
             }
 
-            AccountData.Username = user;
-            AccountData.Password = pass;
-
-            MessageBox.Show("Đăng ký thành công!", "Thông báo",
-                MessageBoxButtons.OK, MessageBoxIcon.Information);
-            this.Close();
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
+            bool success = Database.RegisterAccount(user, pass);
+            if (success)
+            {
+                MessageBox.Show("Đăng ký thành công!");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập đã tồn tại!");
+            }
         }
     }
 }
